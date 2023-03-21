8bbf842e6c1e5777b978a0a4f0e7e04fd0f1f5bd
Rahat Mustafa <Rahatmustafa99@gmail.com>
2023-03-21 18:17:41 -0400

    pexels test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { Client, GatewayIntentBits } = require('discord.js')
+const { createClient } = require('pexels')
 require('dotenv/config')
 
 const client = new Client({
@@ -19,4 +20,28 @@ client.on("messageCreate", message => {
     }
 })
 
+client.on("messageCreate", message => {
+    if (message.content === 'shutdown') {
+        client.destroy();
+    }
+})
+
 client.login(process.env.TOKEN)
+
+
+//import {pclient, query} from "./pexelsModule.mjs";
+
+const pclient = createClient('bxJPwwP46cSPiYtwR72PSPfwdKIxH56nWXKWGpspTcQ1IEbVStVYBQKd');
+const query = 'Nature';
+
+client.on("messageCreate", message => {
+    if (message.content === 'query') {
+        message.reply(query);
+    }
+})
+
+client.on("messageCreate", message => {
+    if (message.content === 'generate') {
+        pclient.photos.search({ query, per_page: 1 }).then(photos => {});
+    }
+})
---
+++ pexelsModule.js
@@ -0,0 +1,9 @@
+
+//import { createClient } from 'pexels';
+
+//const { createClient } = require('pexels')
+
+//const pclient = createClient('bxJPwwP46cSPiYtwR72PSPfwdKIxH56nWXKWGpspTcQ1IEbVStVYBQKd');
+//const query = 'Nature';
+
+//pclient.photos.search({ query, per_page: 1 }).then(photos => {});
